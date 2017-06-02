84ee30f420fe53b8f5658557f14b2cc445ac7b92
ElkynEnriquez <efenriquezm@gmail.com>
2017-06-02 01:38:02 -0500

    Update listar.php

--- listar.php
+++ listar.php
@@ -1,25 +1,24 @@
 <form class="row light-blue-text text-accent-3 center-align blue-grey darken-2" id="formBuscar" name="formBuscar" method="post" action="">
 	<div class="row">			
-		<div class="col s1 hide-on-small-only"></div><div class="col s11"><h4 class="light-blue-text text-darken-1">Buscar estudiante</h4></div>
+		<div class="col s1 hide-on-small-only"></div><div class="col s11"><h3 class="light-blue-text text-darken-2">Buscar estudiante</h3></div>
 	</div>
 	<input type="hidden" name="pag" value="2">
 	<div class="row light-blue-text text-accent-3 center-align">
-		<div class="input-field col s12 m7 l4">
-          <i class="material-icons prefix">account_circle</i>
-          <input id="icon_prefix" type="text" class="validate" name="cod_estudiante">
-          <label for="icon_prefix">Código estudiantil</label>
+		<div class="input-field col s12 m4">
+          <input id="codigo" type="text" class="validate" name="cod_estudiante">
+          <label for="codigo">Código estudiantil</label>
         </div>
-        <div class="input-field col s12 m6 l4">
+        <div class="input-field col s12 m4">
           <input id="nombre" type="text" class="validate" name="nombre">
           <label for="nombre">Nombre</label>
         </div>
-        <div class="input-field col s12 m6 l4">
+        <div class="input-field col s12 m4">
           <input id="apellidos" type="text" class="validate" name="apellidos">
           <label for="apellidos">Apellidos</label>
         </div>
    </div>
    <div class="row light-blue-text text-accent-3 center-align">
-        <div class="col s6 m4">
+        <div class="col s6 m3">
         <div class="input-field">
 		    <select name="cod_grado">
 		      <option value="" disabled selected>Grado</option>
@@ -44,7 +43,7 @@
 		  <label class="" for="fecha_nacimiento">Fecha de nacimiento</label>
           <input id="fecha_nacimiento" type="date" class="datepicker" name="fecha_nacimiento">
 		  </div>
-        <div class="col s6 m4 center-align">
+        <div class="col s6 m3 center-align">
         	<p>Sexo:</p>
 			<p>
 				<input name="cod_genero" type="radio" id="femenino" value="F" />
@@ -53,7 +52,7 @@
 			  	<label for="masculino" class="light-blue-text text-accent-2">Masculino</label>
 			</p>
 			</div>
-		<div class="col s6 m4 center-align">
+		<div class="col s6 m3 center-align">
 			<p>Sede:</p>
 			<p>
 			<input name="cod_sede" type="radio" id="sede1" value="C" />
@@ -62,6 +61,17 @@
 		  	<label for="sede2" class="light-blue-text text-accent-2">Miraflores</label>
 		  	<input name="cod_sede" type="radio" id="sede3" value="L" />
 		  	<label for="sede3" class="light-blue-text text-accent-2">Lorenzo</label>
+		  	</p>
+		</div>
+		<div class="col s6 m3 center-align">
+			<p>Jornada:</p>
+			<p>
+			<input name="cod_jornada" type="radio" id="jornada1" value="M" />
+		  	<label for="jornada1" class="light-blue-text text-accent-2">Mañana</label>
+		  	<input name="cod_jornada" type="radio" id="jornada2" value="T" />
+		  	<label for="jornada2" class="light-blue-text text-accent-2">Tarde</label>
+		  	<input name="cod_jornada" type="radio" id="jornada3" value="N" />
+		  	<label for="jornada3" class="light-blue-text text-accent-2">Noche *Lorenzo</label>
 		  	</p>
 		</div>
 		<div class="col s12">
@@ -73,10 +83,7 @@
 <br><br>
 
 <div class="row">
-	<div class="col s12 l3 center-align"><h5 class="light-blue-text text-accent-2">Semestre</h5></div>
-	<div class="col s12 l3 center-align"><h5 class="light-blue-text text-accent-2">Asignatura</h5></div>
-	<div class="col s12 l3 center-align"><h5 class="light-blue-text text-accent-2">Nombres</h5></div>
-	<div class="col s12 l3 center-align"><h5 class="light-blue-text text-accent-2">Apellidos</h5></div>
+	<div class="col s12 center-align"><h3 class="blue-grey-text text-accent-1">Resultado de la búsqueda</h3></div>
 </div>
 <?php
 $consulta=""; $contC=0;
@@ -102,22 +109,33 @@ foreach($_POST as $n =>$Valor){
 				case "December,": $fecha[1]=12; break;}
 			$consulta=$consulta.$n."='".$fecha[2]."-".$fecha[1]."-".$fecha[0]."'";
 		}
+		elseif($n=="cod_grado") $consulta=$consulta."grado.".$n."=".$Valor;
+		elseif($n=="cod_sede") $consulta=$consulta."sede.".$n."='".$Valor."'";
+		elseif($n=="cod_jornada") $consulta=$consulta."jornada.".$n."='".$Valor."'";
 		elseif(is_numeric($Valor))
 		$consulta=$consulta.$n."=".$Valor;
 		else $consulta=$consulta.$n."='".$Valor."'";
 		$contC++;
 	}
 }
-	$sql2="select * from estudiante".$consulta;
+	if(isset($_POST["cod_grado"])) 
+	$sql2="select estudiante.* from estudiante,curso,grado".$consulta." AND estudiante.cod_curso=curso.cod_curso AND curso.cod_grado=grado.cod_grado";
+	elseif(isset($_POST["cod_sede"])) 
+	$sql2="select estudiante.* from estudiante,curso,sede".$consulta." AND estudiante.cod_curso=curso.cod_curso AND curso.cod_sede=sede.cod_sede";
+	elseif(isset($_POST["cod_jornada"])) 
+	$sql2="select estudiante.* from estudiante,curso,jornada".$consulta." AND estudiante.cod_curso=curso.cod_curso AND curso.cod_jornada=jornada.cod_jornada";
+	else $sql2="select * from estudiante".$consulta;
 	echo $sql2;
 	require("conexion.php");
 	$r2=mysqli_query($conex,$sql2);
-	echo "<form method='post' enctype='multipart/form-data'>";
-	echo "<table class='responsive-table' width=100% border=1px><thead><tr><th>Código</th><th>Nombre</th><th>Apellido</th><th>Fecha de Nacimiento</th><th>Sexo</th><th>Curso</th></tr></thead><tbody>";
-	while ($d=mysqlI_fetch_row($r2)) {
-   	echo "<tr><td>$d[0]</td><td>$d[1]</td><td> $d[2]</td><td>$d[3]</td><td>$d[4]</td><td> $d[5]</td>";    
+	if($r2!=null) {
+		echo "<table class='responsive-table' width=100% border=1px><thead class='light-blue-text text-accent-2'><tr><th>Código</th><th>Nombre</th><th>Apellido</th><th>Fecha de Nacimiento</th><th>Sexo</th><th>Curso</th></tr></thead><tbody>";
+		while ($d=mysqlI_fetch_row($r2)) {
+	   	echo "<tr><td>$d[0]</td><td>$d[1]</td><td> $d[2]</td><td>$d[3]</td><td>$d[4]</td><td> $d[5]</td>";    
+		}
+	echo "</tbody></table>";
 	}
-	echo "</tbody></table></form>"
+	else echo "<center><h1>No hay resultados que mostrar</h1></center>";
 ?>
 <script type="text/javascript">
 	$('.datepicker').pickadate({
